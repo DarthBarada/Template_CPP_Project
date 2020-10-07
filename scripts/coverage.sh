a5eca46c61db3e4e3758a7576c62f38524415a22
destroyer <destroyer@localhost.localdomain>
2020-10-07 22:59:40 +0300

    Troubleshooting

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,7 +4,7 @@ set -ex
 
 ./scripts/tests.sh
 
-cmake -H. -B .build -DBUILD_TESTS=On -DBUILD_COVERAGE=On
+cmake -H. -B .build -DBUILD_COVERAGE=On
 cmake --build .build --target gcov
 cmake --build .build --target test
 gcovr -r  .
